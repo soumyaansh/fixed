163979ab7053c3e24d9d4f377cd23ae1ad151ade
Soumyaansh <soumyaansh@silverminegroup.com>
2018-04-17 01:13:26 +0530

    finale

---
+++ final_1/app/src/main/java/com/example/soumyaanshroy/final_1/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.soumyaanshroy.final_1;
+
+/**
+ * Created by Soumyaansh Roy on 16-04-2018.
+ */
+
+public class LoginActivity {
+}
---
+++ final_1/app/src/main/java/com/example/soumyaanshroy/final_1/SignupActivity.java
@@ -0,0 +1,8 @@
+package com.example.soumyaanshroy.final_1;
+
+/**
+ * Created by Soumyaansh Roy on 17-04-2018.
+ */
+
+public class SignupActivity {
+}
